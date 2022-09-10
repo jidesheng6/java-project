bb8683c26c8800caf9f554c0a6b1c730b0c8980c
jidesheng <2732423664@qq.com>
2022-09-10 12:33:08 +0800

    Changes

---
+++ src/main/java/com/example/jenkinsspring/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.jenkinsspring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping("/msg")
+    public String test(){
+        return "测试OK";
+    }
+}
