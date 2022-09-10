042430fc8d47b4e20cffd502736f1d43d1c40df5
jidesheng <2732423664@qq.com>
2022-09-10 14:57:55 +0800

    Changes

--- src/main/java/com/example/jenkinsspring/controller/MainController.java
+++ src/main/java/com/example/jenkinsspring/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @RequestMapping("/msg")
     public String test(){
-        return "测试OK";
+        return "测试";
     }
 }
